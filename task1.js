3bbbbcdc4a77e6b04dcf0c93974d8c157e42e9b0
rahad0012 <bluesky90987@gmail.com>
2026-02-05 10:30:04 +0600

    First problem solved

--- task1.js
+++ task1.js
@@ -5,3 +5,10 @@ Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+let burgerPrice = 400;
+if (burgerPrice >= 500) {
+    console.log('Enjoy free coke');
+}
+else {
+    console.log('coke = 30 tk')
+}
